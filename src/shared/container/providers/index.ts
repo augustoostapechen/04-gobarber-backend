5eec4c1af964d671653291d1cfa621edb2847472
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-07-17 06:36:24 -0300

    Created email template

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import DiskStorageProvider from './StorageProvider/implamentations/DiskStoragePr
 import EtherealMailProvider from './MailProvider/implamentations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );

0b12ce5a070c35d44bc518d77d0a9515ee0d6887
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-07-30 08:29:44 -0300

    Data validation with celebrate.

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAthenticated';
 
@@ -9,7 +10,18 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.get('/', profileController.show);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required,
+      email: Joi.string().email().required,
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.show,
+);
 profileRouter.put('/', profileController.update);
 
 export default profileRouter;
